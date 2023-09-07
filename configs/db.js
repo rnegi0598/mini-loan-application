004a6e1165795b8298ddd7282477266f8eab0f66
rnegi0598 <45715802+rnegi0598@users.noreply.github.com>
2023-09-07 22:14:51 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB_URL ="mongodb://localhost:27017";
+const DB_URL =process.env.DB_URL;
 const dbConnect = async () => {
   try {
     await mongoose.connect(DB_URL, {
